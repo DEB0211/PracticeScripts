641c51b3b9b344d23a22a07a15863737c5dd7985
Admin <Admin@DESKTOP-3321287>
2021-11-15 13:55:13 +0530

    ADDED NEW

---
+++ src/practice/kav.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class kav {
+	
+	private void kavi() {
+		
+		System.out.println("Hi Kavya");
+		System.out.println("Hi Kavya");
+		// TODO Auto-generated method stub
+
+	}
+
+}
